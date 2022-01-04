f36dbbcdff561e79da21e206dc377f8713abea0b
Matheus-H-Mx <74945355+Matheus-H-Mx@users.noreply.github.com>
2022-01-04 01:48:47 -0300

    1 FIRST CELL FUNCTIONING WORK

---
+++ InnovationTasks/Controllers/TasksTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TasksTableViewController.swift
+//  InnovationTasks
+//
+//  Created by Matheus Henrique on 04/01/22.
+//
+
+import UIKit
+
+class TasksTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+}
