f4bf45f0a57b75439fd94d10676ff00578e37bb4
Matheus-H-Mx <74945355+Matheus-H-Mx@users.noreply.github.com>
2022-01-10 02:04:49 -0300

    UITableViewDateSource editing 1

--- InnovationTasks/Controllers/TasksTableViewController.swift
+++ InnovationTasks/Controllers/TasksTableViewController.swift
@@ -7,6 +7,14 @@
 
 import UIKit
 
+
+let category = Category(name: "Work", color: UIColor.green)
+let tasks: [Task]  = [
+    Task(name: "This is my shoopping list task", date: Date(), category: category),
+    Task(name: "Call Jane About the substitute", date: Date(), category: category),
+]
+
+
 class TasksTableViewController: UITableViewController {
 
     override func viewDidLoad() {
