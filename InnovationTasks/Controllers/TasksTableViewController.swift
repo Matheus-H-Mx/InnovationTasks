e0c8a235cd0ad464942ba5cf8b2cd5555f3752d5
Matheus-H-Mx <74945355+Matheus-H-Mx@users.noreply.github.com>
2022-01-15 23:58:38 -0300

    teste

--- InnovationTasks/Controllers/TasksTableViewController.swift
+++ InnovationTasks/Controllers/TasksTableViewController.swift
@@ -19,7 +19,13 @@ class TasksTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+    }
+    
+    override func tableView (_ tableView:UITableView, numberOfRowsInSection section: Int) -> Int{
+        return  tasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
---
+++ InnovationTasks/Controllers/cells/TaskTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TaskTableViewCell.swift
+//  InnovationTasks
+//
+//  Created by Matheus Henrique on 10/01/22.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+
+}
