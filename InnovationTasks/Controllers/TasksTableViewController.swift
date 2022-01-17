40e8f2d27db3fcb96e7c84c53b9d8f5b4608851d
Matheus-H-Mx <74945355+Matheus-H-Mx@users.noreply.github.com>
2022-01-17 03:59:32 -0300

    Cells of tableView

--- InnovationTasks/Controllers/TasksTableViewController.swift
+++ InnovationTasks/Controllers/TasksTableViewController.swift
@@ -8,15 +8,19 @@
 import UIKit
 
 
-let category = Category(name: "Work", color: UIColor.green)
+let work = Category(name: "Work", color: UIColor.green)
+let study = Category(name: "Study", color: UIColor.red)
 let tasks: [Task]  = [
-    Task(name: "This is my shoopping list task", date: Date(), category: category),
-    Task(name: "Call Jane About the substitute", date: Date(), category: category),
+    Task(name: "create code to tomorrow", date: Date(), category: work),
+    
+    Task(name: "Study math for my computer science class", date: Date(), category: study),
 ]
 
 
 class TasksTableViewController: UITableViewController {
 
+private var dateFormater: DateFormatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -26,6 +30,22 @@ class TasksTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath) as! TaskTableViewCell
+        let task = tasks[indexPath.row]
+        
+        
+        dateFormater.dateFormat = "HH:mm"
+        cell.hourLabel.text = dateFormater.string(from: task.date)
+        
+        dateFormater.dateFormat = "dd/MM/yyyy"
+        cell.dateLabel.text = dateFormater.string(from: task.date)
+        
+        cell.categoryNameLabel.text = task.category.name
+        cell.categoryView.backgroundColor = task.category.color
+        cell.taskDescriptionLabel.text = task.name
+        
+        
+        return cell
+        
     }
 }
--- InnovationTasks/Controllers/cells/TaskTableViewCell.swift
+++ InnovationTasks/Controllers/cells/TaskTableViewCell.swift
@@ -9,5 +9,10 @@ import UIKit
 
 class TaskTableViewCell: UITableViewCell {
 
-
+    @IBOutlet weak var hourLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var categoryNameLabel: UILabel!
+    @IBOutlet weak var taskDescriptionLabel: UILabel!
+    @IBOutlet weak var categoryView: UIView!
+    
 }
