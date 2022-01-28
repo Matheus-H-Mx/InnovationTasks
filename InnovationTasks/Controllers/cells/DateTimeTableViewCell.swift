75298ab4129c9e9605c7f146cf8e350edc3db186
Matheus-H-Mx <74945355+Matheus-H-Mx@users.noreply.github.com>
2022-01-27 23:44:04 -0300

    test 1

---
+++ InnovationTasks/Controllers/cells/DateTimeTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  DateTimeTableViewCell.swift
+//  InnovationTasks
+//
+//  Created by Matheus Henrique on 21/01/22.
+//
+
+import UIKit
+
+class DateTimeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dateTimeTextField: UITextField!
+  
+
+}
---
+++ InnovationTasks/Controllers/cells/TaskDescriptionTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TaskDescriptionTableViewCell.swift
+//  InnovationTasks
+//
+//  Created by Matheus Henrique on 21/01/22.
+//
+
+import UIKit
+
+class TaskDescriptionTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var TaskDescriptionTextField: UITextField!
+    
+}
